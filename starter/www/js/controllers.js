ed663c6c7d7af020e2b46add0ae6659b2be01b8b
Kevin Perkins <kperkins6@mail.csuchico.edu>
2016-04-10 12:20:15 -0700

    First Commit

---
+++ starter/www/js/controllers.js
@@ -0,0 +1,74 @@
+angular.module('app.controllers', [])
+  
+.controller('scheduleEventCtrl', function($scope) {
+
+})
+   
+.controller('myEventsCtrl', function($scope) {
+
+})
+   
+.controller('nearMeCtrl', function($scope) {
+
+})
+         
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('searchBusinessCardsCtrl', function($scope) {
+
+})
+   
+.controller('addTagsCtrl', function($scope) {
+
+})
+   
+.controller('decksCtrl', function($scope) {
+
+})
+   
+.controller('viewDeckCtrl', function($scope) {
+
+})
+   
+.controller('viewBusinessCardCtrl', function($scope) {
+
+})
+   
+.controller('myCardCtrl', function($scope) {
+
+})
+   
+.controller('uploadImageCtrl', function($scope) {
+
+})
+   
+.controller('eventCtrl', function($scope) {
+
+})
+   
+.controller('studyCtrl', function($scope) {
+
+})
+   
+.controller('eventSearchResultsCtrl', function($scope) {
+
+})
+   
+.controller('studyDeckCtrl', function($scope) {
+
+})
+   
+.controller('studyResultsCtrl', function($scope) {
+
+})
+   
+.controller('searchAttendeesCtrl', function($scope) {
+
+})
+ 
